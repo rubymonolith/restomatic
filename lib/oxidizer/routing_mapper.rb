cb48879fa3268cb55039371583f43521f885a1a7
Brad Gessler <bradgessler@gmail.com>
2022-11-07 01:03:52 -0800

    Add nest_namespace

--- lib/oxidizer/routing_mapper.rb
+++ lib/oxidizer/routing_mapper.rb
@@ -3,6 +3,24 @@ module ActionDispatch::Routing
   # more weird stuff. Rails could use an API for adding route helpers to decrease the brittleness of this
   # approach. For now, deal with this helper.
   class Mapper
+    def nest(name = nil, *args, except: nil, **kwargs, &block)
+      assert_resource_scope method_name: :nest
+
+      if name.nil?
+        scope module: parent_resource.name, &block
+      elsif is_singular_resource_name? name
+        scope module: parent_resource.name do
+          except ||= %i[index edit update destroy]
+          resource name, *args, except: except, **kwargs, &block
+        end
+      else
+        scope module: parent_resource.name do
+          except ||= %i[show edit update destroy]
+          resources name, *args, except: except, **kwargs, &block
+        end
+      end
+    end
+
     def create(name, *args, **kwargs, &)
       assert_resource_scope method_name: :create
       nest { inflect_resource_plurality name, *args, **kwargs, & }
@@ -28,20 +46,11 @@ module ActionDispatch::Routing
       nest { inflect_resource_plurality name, *args, **kwargs, &block }
     end
 
-    def nest(name = nil, *args, except: nil, **kwargs, &block)
-      assert_resource_scope method_name: :nest
-
-      if name.nil?
-        scope module: parent_resource.name, &block
-      elsif is_singular_resource_name? name
-        scope module: parent_resource.name do
-          except ||= %i[index edit update destroy]
-          resource name, *args, except: except, **kwargs, &block
-        end
-      else
-        scope module: parent_resource.name do
-          except ||= %i[show edit update destroy]
-          resources name, *args, except: except, **kwargs, &block
+    def nest_namespace(name, *args, **kwargs, &block)
+      assert_resource_scope method_name: :nest_namespace
+      collection do
+        namespace parent_resource.name do
+          namespace name, *args, **kwargs, &block
         end
       end
     end
